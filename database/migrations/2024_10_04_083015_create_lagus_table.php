fb38c266c4d4dd6886e381dd16e5287333e8b9a3
BintangAkbar15 <bintangakbar154@gmail.com>
2024-10-04 16:13:02 +0700

    menambahkan table dan databse

---
+++ database/migrations/2024_10_04_083015_create_lagus_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('lagu', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',100);
+            $table->string('audio');
+            $table->string('thumb');
+            $table->string('slug',50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('lagus');
+    }
+};
