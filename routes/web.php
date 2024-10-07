050d185786a8566a41489ef318dc8aed8fa1ef47
Shilycia <bintangputraadryan103@gmail.com>
2024-10-07 14:29:27 +0700

    register

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,14 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('login');
+    return view('welcome');
 });
+
+Route::get('/register', function () {
+    return view('register');
+})->name('register');
+
+
+Route::get('/login', function () {
+    return view('login');
+})->name('login');
