99698f9f468dc8de4c77fd7a0c42aafe8399cd30
Shilycia <bintangputraadryan103@gmail.com>
2024-10-07 17:38:50 +0700

    register fix login fix

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -25,40 +25,39 @@
                     <h3 for="fw-bold">Bara FM</h3>
                 </a>
             </div>
-            <h1 class="auth-title">Log in.</h1>
-            <p class="auth-subtitle mb-5">Log in for enjoy your save music.</p>
+            <h2 class="">Log in.</h1>
+            <p class="mb-3">Log in for enjoy your save music.</p>
 
             <form action="index.html">
                 <div class="form-group position-relative has-icon-left mb-4">
-                    <input type="text" class="form-control form-control-xl" placeholder="Username">
+                    <input type="text" class="form-control form-control-lg" placeholder="Username">
                     <div class="form-control-icon">
                         <i class="bi bi-person"></i>
                     </div>
                 </div>
                 <div class="form-group position-relative has-icon-left mb-4">
-                    <input type="password" class="form-control form-control-xl" placeholder="Password" id="password-field">
+                    <input type="password" class="form-control form-control-lg" placeholder="Password" id="password-field">
                     <div class="form-control-icon">
                         <i class="bi bi-shield-lock"></i>
                     </div>
                 </div>
-                <div class="form-check form-check-lg d-flex align-items-end" id="container-showpass">
+                <div class="form-check form-check-md d-flex align-items-end" id="container-showpass">
                     <input class="form-check-input me-2" type="checkbox" value="" id="showpass">
                     <label class="form-check-label text-gray-600" for="showpass">
                         Show Password
                     </label>
                 </div>
-                <button class="btn btn-primary btn-block btn-lg shadow-lg mt-5">Log in</button>
+                <button class="btn btn-primary btn-block btn-md shadow-lg mt-5">Log in</button>
             </form>
-            <div class="text-center mt-5 text-lg fs-4">
+            <div class="text-center mt-2">
                 <p class="text-gray-600">Don't have an account? <a href="{{ route('register') }}" class="font-bold">Sign
                         up</a>.</p>
-                <p><a class="font-bold" href="auth-forgot-password.html">Forgot password?</a>.</p>
+                <p><a class="font-bold" href="#">Forgot password?</a>.</p>
             </div>
         </div>
     </div>
     <div class="col-lg-7 d-none d-lg-block">
-        <div id="auth-right">
-
+        <div id="auth-right" style="background: url('{{ url('img/dumpimg.png') }}');">
         </div>
     </div>
 </div>
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -18,43 +18,41 @@
 <div class="row h-100">
     <div class="col-lg-5 col-12">
         <div id="auth-left">
-            <div class="auth-logo">
-                <a href="index.html" class="d-flex gap-3">
-                    <img src="{{ asset('img/logo.png') }}" alt="Logo">
-                    <h3 for="fw-bold">Bara FM</h3>
-                </a>
-            </div>
-            <h1 class="auth-title">Sign Up</h1>
-            <p class="auth-subtitle mb-5">Input your data to register to our website.</p>
+            <a href="index.html" class="d-flex gap-3">
+                <img src="{{ asset('img/logo.png') }}" width="40px" alt="Logo">
+                <h3 for="fw-bold">Bara FM</h3>
+            </a>
+            <h1 class="mt-5">Sign Up</h1>
+            <p class="mb-5">Input your data to register to our website.</p>
 
             <form action="index.html">
                 <div class="form-group position-relative has-icon-left mb-4">
-                    <input type="text" class="form-control form-control-xl" placeholder="Email">
+                    <input type="text" class="form-control form-control-md" placeholder="Email">
                     <div class="form-control-icon">
                         <i class="bi bi-envelope"></i>
                     </div>
                 </div>
                 <div class="form-group position-relative has-icon-left mb-4">
-                    <input type="text" class="form-control form-control-xl" placeholder="Username">
+                    <input type="text" class="form-control form-control-md" placeholder="Username">
                     <div class="form-control-icon">
                         <i class="bi bi-person"></i>
                     </div>
                 </div>
                 <div class="form-group position-relative has-icon-left mb-4">
-                    <input type="password" class="form-control form-control-xl" placeholder="Password">
+                    <input type="password" class="form-control form-control-md" placeholder="Password">
                     <div class="form-control-icon">
                         <i class="bi bi-shield-lock"></i>
                     </div>
                 </div>
                 <div class="form-group position-relative has-icon-left mb-4">
-                    <input type="password" class="form-control form-control-xl" placeholder="Confirm Password">
+                    <input type="password" class="form-control form-control-md" placeholder="Confirm Password">
                     <div class="form-control-icon">
                         <i class="bi bi-shield-lock"></i>
                     </div>
                 </div>
-                <button class="btn btn-primary btn-block btn-lg shadow-lg mt-5">Sign Up</button>
+                <button class="btn btn-primary btn-block btn-md shadow-lg mt-2">Sign Up</button>
             </form>
-            <div class="text-center mt-5 text-lg fs-4">
+            <div class="text-center mt-3 fs-6">
                 <p class='text-gray-600'>Already have an account? <a href="{{ route('login') }}" class="font-bold">Log
                         in</a>.</p>
             </div>
