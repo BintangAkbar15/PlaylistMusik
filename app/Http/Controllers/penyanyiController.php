b0c3387686fa72dc266c96a88868fd69ffa93503
BintangAkbar15 <bintangakbar154@gmail.com>
2024-10-09 11:15:50 +0700

    menambahkan kontroller

---
+++ app/Http/Controllers/penyanyiController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class penyanyiController extends Controller
+{
+    function store(){
+
+    }
+    function index(){
+
+    }
+    function update(){
+
+    }
+    function destroy(){
+
+    }
+}
