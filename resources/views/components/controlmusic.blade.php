f8dd8c05c9e5ab1f3c7ef32573092591342528c8
Shilycia <bintangputraadryan103@gmail.com>
2024-10-14 11:49:33 +0700

    controller music

--- resources/views/components/controlmusic.blade.php
+++ resources/views/components/controlmusic.blade.php
@@ -1,32 +1,37 @@
 <footer class="fixed-bottom  bg-dark col-12 d-flex pt-3" style="height: 15vh">
     <div class="col-8 col-md-3 d-flex align-items-center ps-4 gap-3" style="height:12vh;">
-        <img src="{{ url('img/dumpimg.png') }}" style="width: 70px; height: 70px;" class="rounded d-none d-md-block" alt="">
+        <img src="{{ url('data\photo\michino thimoty kimochi.jpeg') }}" style="width: 70px; height: 70px;" class="rounded d-none d-md-block" alt="">
         <div class="d-flex flex-column gap-1 overflow-hidden">
-            <label for="" class="fs-4 text-nowrap">Semua aku di rayakan</label>
-            <label for="" class="fs-6 text-nowrap">artis pergi tak pulang</label>
+            <label for="" class="fs-4 text-nowrap">Michino Timothy Kimino Kimochi</label>
+            <label for="" class="fs-6 text-nowrap">Anne Happy</label>
         </div>
     </div>
     <div class="col-4 col-md-9 col-lg-6 px-sm-5 pe-2 pe-md-0 d-flex flex-column align-items-center">
         <div class="d-flex align-items-center d-none  d-md-flex justify-content-center w-100">
-            <label for="customRange1" class="me-2 mb-0 text-nowrap">-:--</label> 
-            <input type="range" class="form-range px-4 flex-grow-1" id="customRange1"> 
-            <label for="customRange1" class="ms-2 me-4 me-lg-0 mb-0 text-nowrap">-:--</label> 
+            <label for="customRange1" class="me-2 mb-0 text-nowrap" id="progres-time">-:--</label> 
+            <input type="range" class="form-range px-4 flex-grow-1" id="customRange1" value="0"> 
+            <label for="customRange1" class="ms-2 me-4 me-lg-0 mb-0 text-nowrap" id="end-time">-:--</label> 
+            <audio id="my-audio" class="d-none">
+                <source src="{{ url('data\MP3\ONE PROJECT & mei - caramel ribbon cursetard.mp3') }}" type="audio/mp3">
+            </audio>
         </div>        
         <div class="d-flex align-items-center justify-content-end justify-content-md-center gap-3 gap-md-5" style="height: 12vh; font-size: 30px;">
-            <i class="fa-solid fa-shuffle d-none d-md-block"></i> 
-            <i class="fa-solid fa-backward-step"></i>
-            <i class="fa-solid fa-pause"></i>
-            <i class="fa-solid fa-forward-step"></i>
-            <i class="fa-solid fa-repeat d-none d-md-block"></i> 
+            <i class="fa-solid fa-shuffle d-none d-md-block pe-auto" id="shuffle"></i> 
+            <i class="fa-solid fa-backward-step pe-auto" id="prev"></i>
+            <i class="fa-solid fa-play pe-auto" id="play-pause"></i>
+            <i class="fa-solid fa-forward-step pe-auto" id="next"></i>
+            <i class="fa-solid fa-repeat d-none d-md-block pe-auto" id="repeat"></i> 
         </div>
     </div>
     
     <div class="col-3 gap-3 d-none d-lg-flex justify-content-end align-items-center pe-4">
-        <i class="fa-solid fa-list"></i>
+        <i class="fa-regular fa-heart fs-4 pe-auto" style="color: white;" id="like-btn"></i>
+        <i class="fa-solid fa-list pe-auto" id="queue"></i>
         <div class="d-flex align-items-center">
-            <i class="fa-solid fa-volume-high me-2"></i>
-            <input type="range">
+            <i class="fa-solid fa-volume-high me-2 pe-auto"></i>
+            <input type="range" id="volume" value='100'>
         </div>
-        <i class="fa-solid fa-up-right-and-down-left-from-center"></i>
-    </div>    
+        <i class="fa-solid fa-up-right-and-down-left-from-center pe-auto" id="maximaze"></i>
+    </div>
+    <script src="{{ asset('js/controlmusic.js') }}"></script>
 </footer>  
--- resources/views/components/infopage.blade.php
+++ resources/views/components/infopage.blade.php
@@ -1,14 +1,14 @@
 <div class="col-2 p-1 d-none d-lg-block">
     <div class="col-12 rounded bg-secondary p-3 bg-opacity-10 h-100 d-flex flex-column overflow-y-auto">
-        <img src="{{ url('img/dumpimg.png') }}" alt="" width="100%" class="rounded">
+        <img src="{{ url('data\photo\michino thimoty kimochi.jpeg') }}" alt="" width="100%" class="rounded">
         <marquee behavior="" direction="" class="col-12 overflow-hidden" scrollamount='3'>
-            <label for="" class="fs-3 mt-2 text-nowrap fw-bold text-white">Semua aku di rayakan</label>
+            <label for="" class="fs-3 mt-2 text-nowrap fw-bold text-white">Michino Timothy Kimino Kimochi</label>
         </marquee>
-        <label for="" class="fs-6 overflow-hidden col-12 text-nowrap text-white">Artis pergi tak pulang</label>
+        <label for="" class="fs-6 overflow-hidden col-12 text-nowrap text-white">Anne Happy</label>
         <div class="p-3 rounded bg-opacity-50 mt-4" style="background: linear-gradient(to bottom right, rgba(255, 255, 255, 0.5), rgba(128, 128, 128, 0.5)); backdrop-filter: blur(10px); -webkit-backdrop-filter: blur(10px);">
             <div class="col-12 d-flex gap-3">
                 <img src="{{ url('img/dumpimg.png') }}" width="50px" alt="" class="rounded-circle shadow">
-                <label for="" class="text-white">Artis pergi tak pulang<i class="fa-solid fa-circle-check ms-2" style="color: rgb(0, 208, 255);"></i></label>
+                <label for="" class="text-white">Anne Happy<i class="fa-solid fa-circle-check ms-2" style="color: rgb(0, 208, 255);"></i></label>
             </div>
             <div class="col-12 text-end">
                 <button class="py-1 px-3 gap-3 mt-3 btn rounded-pill border border-white text-white">
